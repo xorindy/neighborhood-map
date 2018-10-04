b0a1787b55617c31f97c7cc7662697515c547989
Clarinda Bui <clarindabui@outlook.com>
2018-10-03 21:10:22 -0700

    Add Header

--- src/App.js
+++ src/App.js
@@ -3,25 +3,45 @@ import './App.css';
 
 class App extends Component {
 
+  componentDidMount() {
+    this.loadMap()
+  }
+
+  loadMap = () => {
+    initScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCgQ_AkG66ugXFSqnfk01xYIIlXwDWR3Tc&callback=initMap')
+    window.initMap = this.initMap
+  }
+
   initMap = () => {
-    const map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644}, 
-      zoom: 8
-    });
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 36.169090, lng: -115.140579}, 
+      zoom: 13
+    })
   }
 
   render() {
     return (
-      <main>
+      <div className="app">
+        <header className="app-header">
+          <h1 className="app-title">My Neighborhood Map</h1>
+        </header>
         <div id="map"></div>
-      </main>
+      </div>
       
     );
   }
 }
 
-/* 
-<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY_HERE&callback=initMap" async defer></script>
-*/
+// Function to make the script and load it for the API key
+function initScript(url) {
+  const reference = window.document.getElementsByTagName("script")[0]
+  const script = window.document.createElement("script")
+
+  script.src = url
+  script.async = true
+  script.defer = true
+
+  reference.parentNode.insertBefore(script, reference)
+}
 
 export default App;
