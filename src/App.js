71737cdeadf7695dc0197489ddfeda8860d600e3
Clarinda Bui <clarindabui@outlook.com>
2018-10-04 15:34:08 -0700

    Add Foursquare API

--- src/App.js
+++ src/App.js
@@ -1,57 +1,15 @@
 import React, { Component } from 'react';
+import FoursquareAPI from './api/foursquare';
 import './App.css';
 
 class App extends Component {
 
   componentDidMount() {
-    this.loadMap()
-  }
-
-  loadMap = () => {
-    initScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCgQ_AkG66ugXFSqnfk01xYIIlXwDWR3Tc&callback=initMap')
-    window.initMap = this.initMap
-  }
-
-  initMap = () => {
-    const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 36.1124493, lng: -115.1716128}, 
-      zoom: 13
-    })
-    // Default markers on the map
-    const locations = [
-      {title: 'Sea Quest Las Vegas', location: {lat: 36.12301, lng: -115.1349287}},
-      {title: 'Zak Bagans Haunted Museum', location: {lat: 36.1571324, lng: -115.1472111}},
-      {title: 'Las Vegas Sign', location: {lat: 36.0833733, lng: -115.1737739}},
-      {title: 'Marvel Avengers Station', location: {lat: 36.1250655, lng: -115.1746774}},
-      {title: 'Outdoor Gondola Rides', location: {lat: 36.1170653, lng: -115.1717004}}
-    ]
-
-    let infoWindow = new window.google.maps.InfoWindow()
-
-    // Create array of markers to load
-    for (var i=0; i < locations.length; i++) {
-      
-      // Get positions from locations array
-      let pos = locations[i].location
-      let name = locations[i].title
-
-      // Make marker for each location
-      let marker = new window.google.maps.Marker({
-        map: map,
-        position: pos,
-        title: name,
-        animation: window.google.maps.Animation.DROP,
-        id: i
-      })
-
-      // Push marker to our array of markers
-      markers.push(marker)
-
-      // Create onClick event to open the info window for each marker
-      marker.addListener('click', function() {
-        populateInfoWindow(this, infoWindow)
-      })
-    }
+    FoursquareAPI.search({
+      near: 'Las Vegas, NV',
+      query: 'park',
+      limit: 15
+    });
   }
 
   render() {
@@ -67,34 +25,5 @@ class App extends Component {
   }
 }
 
-// Declare markers array
-const markers = [];
-
-// Function to make the script and load it for the API key
-function initScript(url) {
-  const reference = window.document.getElementsByTagName("script")[0]
-  const script = window.document.createElement("script")
-
-  script.src = url
-  script.async = true
-  script.defer = true
-
-  reference.parentNode.insertBefore(script, reference)
-}
-
- // Populate the info window when marker is clicked.
- function populateInfoWindow(marker, infoWindow) {
-  // Check is there is already an info window open
-  if (infoWindow.marker !== marker) {
-    infoWindow.marker = marker
-    infoWindow.setContent('<div>' + marker.title + '</div>')
-    infoWindow.open(this, marker)
-
-    // Make sure marker is cleared when info window is closed
-    infoWindow.addListener('closeClick', function() {
-      infoWindow.setMarker(null)
-    })
-  }
-}
 
 export default App;
---
+++ src/api/foursquare.js
@@ -0,0 +1,65 @@
+class Helper {
+    static baseURL() {
+        return 'https://api.foursquare.com/v2';
+    }
+
+    static auth() {
+        const keys = {
+            clientID: '1OHRXNFKHBMCPQXLUR32TQ4FG2HCIVDFERWN2RVBFPH34MDH',
+            clientSecret: 'YTCU10YMNIIM2IMMR1M23DXZV021G45EVSWECJHYWLGQU0IP',
+            version: '20181004'
+        };
+
+        // Return keys into an array, and make a string literal of all the keys combined
+        return Object.keys(keys)
+            .map(key => `${key}=${keys[key]}`)
+            .join('&');
+    }
+
+    // Build the URL
+    static urlBuilder(urlParams) {
+        if(!urlParams) { // If empty, return nothing
+            return '';
+        } else {
+            return Object.keys(urlParams)
+                .map(key => `${key}=${urlParams[key]}`)
+                .join('&');
+        }
+            
+    }
+
+    static headers() {
+        return {
+            Accept: 'application/json'
+        };
+    }
+
+    static simpleFetch(endURL, method, urlParams) {
+        let requestData = {
+            method,
+            headers: Helper.headers()
+        };
+
+        return fetch(`${Helper.baseURL()}${endURL}?${Helper.auth()}&${Helper.urlBuilder(urlParams)}`,
+            requestData)
+            .then(response => response.json());
+    }
+}
+
+export default class FoursquareAPI {
+
+    // Return search URL
+    static search(urlParams) {
+        return Helper.simpleFetch('/venues/search', 'GET', urlParams);
+    }
+
+    // Return place detail URL
+    static getPlaceDetails(VENUE_ID) {
+        return Helper.simpleFetch(`/venues/${VENUE_ID}`, 'GET');
+    }
+
+    // Return photos of places
+    static getPlacePhotos(VENUE_ID) {
+        return Helper.simpleFetch(`/venues/${VENUE_ID}/photos`, 'GET');
+    }
+}
