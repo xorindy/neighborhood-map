9e82dd8014d541e43758436e1acb5f3c857f1b1e
Clarinda Bui <clarindabui@outlook.com>
2018-10-04 16:17:24 -0700

    Load map in app

--- src/App.js
+++ src/App.js
@@ -9,21 +9,50 @@ class App extends Component {
       near: 'Las Vegas, NV',
       query: 'park',
       limit: 15
-    });
+    })
+
+    this.loadMap()
+  }
+  
+  
+  loadMap = () => {
+    initScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCgQ_AkG66ugXFSqnfk01xYIIlXwDWR3Tc&callback=initMap')
+    window.initMap = this.initMap
+  }
+  
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 36.1124493, lng: -115.1716128}, 
+      zoom: 13
+    })
   }
 
   render() {
     return (
       <div className="app">
+
         <header className="app-header">
           <h1 className="app-title">My Neighborhood Map</h1>
         </header>
+
         <div id="map"></div>
+        
       </div>
       
     );
   }
 }
 
+// Function to make the script and load it for the API key
+function initScript(url) {
+  const reference = window.document.getElementsByTagName("script")[0]
+  const script = window.document.createElement("script")
+
+  script.src = url
+  script.async = true
+  script.defer = true
+
+  reference.parentNode.insertBefore(script, reference)
+}
 
 export default App;
